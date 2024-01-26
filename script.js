c5cc60fbcf130a8054cf8bd4badb7421ae3b3bb5
Igor Sales <root@friday-len>
2024-01-26 17:10:02 -0300

    ajuste grosso(la ele) em style

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const generateBtn = document.getElementById('generate-btn');
 const card = document.getElementById('card');
-const cardTitle = document.querySelector('.card .card-header .topic-title');
-const cardName = document.querySelector('.card .card-header .topic-name');
-const cardContent = document.querySelector('.card .card-content');
+const tipoElement = document.getElementById('tipo');
+const nomeElement = document.getElementById('nome');
+const conteudoElement = document.getElementById('conteudo');
 
 async function fetchCartas(tipo) {
     const response = await fetch(`Cartas/${tipo.toLowerCase()}.json`);
@@ -15,22 +14,15 @@ async function gerarNovaCarta() {
 
     const { name, content } = await fetchCartas(tipo);
 
-    cardTitle.innerHTML = `
-        <div class="topic-title ${tipo.toLowerCase()}">${tipo}</div>
-    `;
+    tipoElement.textContent = tipo;
+    nomeElement.textContent = name;
 
-    cardName.innerHTML = `
-        <div class="topic-name">${name}</div>
-    `;
-
-    cardContent.innerHTML = content.map((tip, index) => `
+    conteudoElement.innerHTML = content.map((tip, index) => `
         <div class="tip">${index + 1}. ${tip}</div>
     `).join('');
 
     card.className = `card ${tipo.toLowerCase()}`;
 }
-
-generateBtn.addEventListener('click', gerarNovaCarta);
 
 // Chamada inicial para gerar uma carta quando a página é carregada
 gerarNovaCarta();
